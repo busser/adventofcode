5ba7a37e4d97e3911dbea3ecda439c73a67bd503
Arthur Busser <arthur.busser@gopigment.com>
2022-12-11 23:09:28 +0100

    add solution for y2022 d11

---
+++ y2022/d11/solution.go
@@ -0,0 +1,266 @@
+package d11
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/busser/adventofcode/helpers"
+	"github.com/busser/adventofcode/y2022/d11/queue"
+)
+
+// PartOne solves the first problem of day 11 of Advent of Code 2022.
+func PartOne(r io.Reader, w io.Writer) error {
+	monkeys, err := monkeysFromReader(r)
+	if err != nil {
+		return fmt.Errorf("could not read input: %w", err)
+	}
+
+	business := monkeyBusiness(monkeys, 20, true)
+
+	_, err = fmt.Fprintf(w, "%d", business)
+	if err != nil {
+		return fmt.Errorf("could not write answer: %w", err)
+	}
+
+	return nil
+}
+
+// PartTwo solves the second problem of day 11 of Advent of Code 2022.
+func PartTwo(r io.Reader, w io.Writer) error {
+	monkeys, err := monkeysFromReader(r)
+	if err != nil {
+		return fmt.Errorf("could not read input: %w", err)
+	}
+
+	business := monkeyBusiness(monkeys, 10_000, false)
+
+	_, err = fmt.Fprintf(w, "%d", business)
+	if err != nil {
+		return fmt.Errorf("could not write answer: %w", err)
+	}
+
+	return nil
+}
+
+type monkey struct {
+	startingItems  []int
+	items          *queue.Queue[int]
+	operation      func(int) int
+	divisor        int
+	throwToIfTrue  int
+	throwToIfFalse int
+}
+
+func monkeyBusiness(monkeys []monkey, rounds int, relief bool) int {
+	productOfDivisors := 1
+	for i := range monkeys {
+		productOfDivisors *= monkeys[i].divisor
+	}
+
+	// In order to improve memory usage, we queue the items through a ring
+	// buffer. This buffer needs enough capacity to store all items at once, if
+	// need be.
+	totalItems := 0
+	for i := range monkeys {
+		totalItems += len(monkeys[i].startingItems)
+	}
+	for i := range monkeys {
+		monkeys[i].items = queue.New[int](totalItems)
+		for _, item := range monkeys[i].startingItems {
+			monkeys[i].items.Enqueue(item)
+		}
+	}
+
+	business := make([]int, len(monkeys))
+
+	for r := 0; r < rounds; r++ {
+		for i := range monkeys {
+			business[i] += monkeys[i].items.Len()
+
+			for monkeys[i].items.Len() > 0 {
+				item := monkeys[i].items.Dequeue()
+
+				item = monkeys[i].operation(item)
+				if relief {
+					item /= 3
+				}
+
+				// To avoid the level of worry for a given item overflowing, all
+				// the while keeping all divisability results the same, we only
+				// keep the modulus of the worry level and of productOfDivisors.
+				item %= productOfDivisors
+
+				next := monkeys[i].throwToIfFalse
+				if item%monkeys[i].divisor == 0 {
+					next = monkeys[i].throwToIfTrue
+				}
+
+				monkeys[next].items.Enqueue(item)
+			}
+		}
+	}
+
+	sort.Sort(sort.Reverse(sort.IntSlice(business)))
+
+	return business[0] * business[1]
+}
+
+func monkeysFromReader(r io.Reader) ([]monkey, error) {
+	lines, err := helpers.LinesFromReader(r)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(lines)%7 != 6 {
+		return nil, fmt.Errorf("unexpected number of lines")
+	}
+
+	numMonkeys := (len(lines) + 1) / 7
+	if numMonkeys < 2 {
+		return nil, errors.New("not enough monkey to compute monkey business")
+	}
+
+	var monkeys []monkey
+
+	for i := 0; i < len(lines); i += 7 {
+		m, err := monkeyFromLines(lines[i : i+6])
+		if err != nil {
+			return nil, err
+		}
+
+		if m.throwToIfFalse < 0 || m.throwToIfFalse >= numMonkeys {
+			return nil, fmt.Errorf("cannot throw to monkey %d", m.throwToIfFalse)
+		}
+		if m.throwToIfTrue < 0 || m.throwToIfTrue >= numMonkeys {
+			return nil, fmt.Errorf("cannot throw to monkey %d", m.throwToIfTrue)
+		}
+
+		monkeys = append(monkeys, m)
+	}
+
+	return monkeys, nil
+}
+
+func monkeyFromLines(lines []string) (monkey, error) {
+	items, err := itemsFromLine(lines[1])
+	if err != nil {
+		return monkey{}, err
+	}
+
+	operation, err := operationFromLine(lines[2])
+	if err != nil {
+		return monkey{}, err
+	}
+
+	divisor, err := divisorFromLine(lines[3])
+	if err != nil {
+		return monkey{}, err
+	}
+
+	throwToIfTrue, err := throwToIfTrueFromLine(lines[4])
+	if err != nil {
+		return monkey{}, err
+	}
+
+	throwToIfFalse, err := throwToIfFalseFromLine(lines[5])
+	if err != nil {
+		return monkey{}, err
+	}
+
+	return monkey{
+		startingItems:  items,
+		operation:      operation,
+		divisor:        divisor,
+		throwToIfTrue:  throwToIfTrue,
+		throwToIfFalse: throwToIfFalse,
+	}, nil
+}
+
+func itemsFromLine(s string) ([]int, error) {
+	var items []int
+
+	rawItems := strings.TrimPrefix(s, "  Starting items: ")
+	for _, raw := range strings.Split(rawItems, ", ") {
+		item, err := strconv.Atoi(string(raw))
+		if err != nil {
+			return nil, fmt.Errorf("%q is not a number", string(raw))
+		}
+
+		items = append(items, item)
+	}
+
+	return items, nil
+}
+
+func operationFromLine(s string) (func(int) int, error) {
+	rawOperation := strings.TrimPrefix(s, "  Operation: new = old ")
+	parts := strings.SplitN(rawOperation, " ", 2)
+	if len(parts) != 2 {
+		return nil, errors.New("wrong format")
+	}
+
+	operator := parts[0]
+	rawArgument := parts[1]
+
+	if rawArgument == "old" {
+		switch operator {
+		case "+":
+			return func(old int) int { return old + old }, nil
+		case "*":
+			return func(old int) int { return old * old }, nil
+		default:
+			return nil, fmt.Errorf("unknown operator %q", operator)
+		}
+	}
+
+	arg, err := strconv.Atoi(rawArgument)
+	if err != nil {
+		return nil, fmt.Errorf("%q is not a number", rawArgument)
+	}
+
+	switch operator {
+	case "+":
+		return func(old int) int { return old + arg }, nil
+	case "*":
+		return func(old int) int { return old * arg }, nil
+	default:
+		return nil, fmt.Errorf("unknown operator %q", operator)
+	}
+}
+
+func divisorFromLine(s string) (int, error) {
+	raw := strings.TrimPrefix(s, "  Test: divisible by ")
+
+	divisor, err := strconv.Atoi(raw)
+	if err != nil {
+		return 0, fmt.Errorf("%q is not a number", raw)
+	}
+
+	return divisor, nil
+}
+
+func throwToIfTrueFromLine(s string) (int, error) {
+	raw := strings.TrimPrefix(s, "    If true: throw to monkey ")
+
+	throwTo, err := strconv.Atoi(raw)
+	if err != nil {
+		return 0, fmt.Errorf("%q is not a number", raw)
+	}
+
+	return throwTo, nil
+}
+
+func throwToIfFalseFromLine(s string) (int, error) {
+	raw := strings.TrimPrefix(s, "    If false: throw to monkey ")
+
+	throwTo, err := strconv.Atoi(raw)
+	if err != nil {
+		return 0, fmt.Errorf("%q is not a number", raw)
+	}
+
+	return throwTo, nil
+}
