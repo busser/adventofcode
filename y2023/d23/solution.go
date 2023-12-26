8484b8fbc47c0f8c7eeed384931490bdeefda562
Arthur Busser <arthur.busser@gopigment.com>
2023-12-26 13:28:00 +0100

    improve solution for y2023 d23

--- y2023/d23/solution.go
+++ y2023/d23/solution.go
@@ -14,7 +14,11 @@ func PartOne(r io.Reader, w io.Writer) error {
 		return fmt.Errorf("could not read input: %w", err)
 	}
 
-	length := longestPath(hikingMap)
+	start := vector{0, 1}
+	end := vector{len(hikingMap) - 1, len(hikingMap[0]) - 2}
+
+	g := graphFromMap(hikingMap, start, end)
+	length := longestPath(g, start, end)
 
 	_, err = fmt.Fprintf(w, "%d", length)
 	if err != nil {
@@ -31,12 +35,12 @@ func PartTwo(r io.Reader, w io.Writer) error {
 		return fmt.Errorf("could not read input: %w", err)
 	}
 
-	removeSlopes(hikingMap)
+	start := vector{0, 1}
+	end := vector{len(hikingMap) - 1, len(hikingMap[0]) - 2}
 
-	// Returning the answer immidiately to save time when running tests.
-	// The solution runs in about 4 minutes. This could definitely be improved.
-	length := 6802
-	// length := longestPath(hikingMap)
+	removeSlopes(hikingMap)
+	g := graphFromMap(hikingMap, start, end)
+	length := longestPath(g, start, end)
 
 	_, err = fmt.Fprintf(w, "%d", length)
 	if err != nil {
@@ -70,66 +74,150 @@ func (v vector) add(w vector) vector {
 	return vector{v.row + w.row, v.col + w.col}
 }
 
-func longestPath(m [][]byte) int {
-	start := vector{0, 1}
-	end := vector{len(m) - 1, len(m[0]) - 2}
+type graph map[vector][]graphEdge
 
-	visited := make([][]bool, len(m))
-	for row := range visited {
-		visited[row] = make([]bool, len(m[row]))
+type graphEdge struct {
+	to     vector
+	length int
+}
+
+func graphFromMap(m [][]byte, start, end vector) graph {
+	// This graph's nodes are only the positions we want to keep. We choose
+	// these nodes to be the positions where the path can take multiple
+	// directions. This ensures that the graph is a small as possible.
+
+	nodesToKeep := make(map[vector]bool)
+
+	nodesToKeep[start] = true
+	nodesToKeep[end] = true
+
+	for row := range m {
+		for col := range m[row] {
+			if m[row][col] == forest {
+				continue
+			}
+
+			possibleDirections := possibleDirectionsFor(m[row][col])
+
+			validNeighbors := 0
+			for _, v := range possibleDirections {
+				next := vector{row, col}.add(v)
+
+				if next.row < 0 || next.row >= len(m) || next.col < 0 || next.col >= len(m[next.row]) {
+					continue
+				}
+				if m[next.row][next.col] == forest {
+					continue
+				}
+
+				validNeighbors++
+			}
+
+			if validNeighbors > 2 {
+				nodesToKeep[vector{row, col}] = true
+			}
+		}
 	}
 
-	length, _ := longestPathFrom(m, visited, start, end)
+	// The edges are the paths from each node to neighboring nodes. The length
+	// of the edge is the number of steps it takes to get from one node to the
+	// other. We don't add edges for paths that go through another node.
+	//
+	// Since each node represents a fork, there cannot be forks on the path
+	// between two nodes. With simple DFS, we can explore from a node to all
+	// neighboring nodes and add the edges to the graph. We stop exploring when
+	// we reach another node.
+
+	g := make(graph)
+
+	for node := range nodesToKeep {
+		visited := make(map[vector]bool)
+		visited[node] = true
+
+		var stack []graphEdge
+		stack = append(stack, graphEdge{node, 0})
+
+		for len(stack) > 0 {
+			current := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			possibleDirections := possibleDirectionsFor(m[current.to.row][current.to.col])
+
+			for _, v := range possibleDirections {
+				next := current.to.add(v)
+
+				if next.row < 0 || next.row >= len(m) || next.col < 0 || next.col >= len(m[next.row]) {
+					continue
+				}
+				if m[next.row][next.col] == forest {
+					continue
+				}
+				if visited[next] {
+					continue
+				}
+				if nodesToKeep[next] {
+					g[node] = append(g[node], graphEdge{next, current.length + 1})
+					continue
+				}
+
+				visited[next] = true
+				stack = append(stack, graphEdge{next, current.length + 1})
+			}
+		}
+	}
+
+	return g
+}
+
+func possibleDirectionsFor(c byte) []vector {
+	switch c {
+	case path:
+		return []vector{up, down, left, right}
+	case upSlope:
+		return []vector{up}
+	case downSlope:
+		return []vector{down}
+	case leftSlope:
+		return []vector{left}
+	case rightSlope:
+		return []vector{right}
+	default:
+		return nil
+	}
+}
+
+func longestPath(g graph, start, end vector) int {
+	visited := make(map[vector]bool)
+
+	length, _ := longestPathFrom(g, visited, start, end)
 
 	return length
 }
 
-func longestPathFrom(m [][]byte, visited [][]bool, start, end vector) (int, bool) {
+func longestPathFrom(g graph, visited map[vector]bool, start, end vector) (int, bool) {
 	if start == end {
 		return 0, true
 	}
 
-	visited[start.row][start.col] = true
+	visited[start] = true
 
-	var possibleDirections []vector
-	switch m[start.row][start.col] {
-	case upSlope:
-		possibleDirections = []vector{up}
-	case downSlope:
-		possibleDirections = []vector{down}
-	case leftSlope:
-		possibleDirections = []vector{left}
-	case rightSlope:
-		possibleDirections = []vector{right}
-	default:
-		possibleDirections = []vector{up, down, left, right}
-	}
-
-	reachedEnd := false
 	var longest int
-	for _, v := range possibleDirections {
-		next := start.add(v)
-
-		if next.row < 0 || next.row >= len(m) || next.col < 0 || next.col >= len(m[next.row]) {
-			continue
-		}
-		if visited[next.row][next.col] {
-			continue
-		}
-		if m[next.row][next.col] == forest {
+	foundPath := false
+	for _, edge := range g[start] {
+		if visited[edge.to] {
 			continue
 		}
 
-		length, complete := longestPathFrom(m, visited, next, end)
-		if complete {
-			reachedEnd = true
-			longest = max(longest, length)
+		length, endReached := longestPathFrom(g, visited, edge.to, end)
+		if endReached {
+			longest = max(longest, length+edge.length)
+			foundPath = true
 		}
 	}
 
-	visited[start.row][start.col] = false
+	visited[start] = false
 
-	return longest + 1, reachedEnd
+	return longest, foundPath
 }
 
 func removeSlopes(m [][]byte) {
